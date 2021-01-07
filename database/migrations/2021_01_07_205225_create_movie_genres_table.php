ec6d0664fd6d281ff565091965f527fd4dd56e82
NSarvari <nabiivanova4@gmail.com>
2021-01-08 00:19:00 +0200

    Movie table add CRUD

---
+++ database/migrations/2021_01_07_205225_create_movie_genres_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovieGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_genres', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('movie_id')->unsigned();
+            $table->integer('genre_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_genres');
+    }
+}
---
+++ database/migrations/2021_01_07_213720_create_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('releaseYear');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
